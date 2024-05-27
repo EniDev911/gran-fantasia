9544460e4d52828fc08dc4caf820729bcafdd810
enidev911 <enidev911@gmail.com>
2024-05-27 05:53:07 -0400

    up clase Personaje

--- personaje.py
+++ personaje.py
@@ -1,9 +1,10 @@
 from mensajes import leer_dialogos
 
+
 class Character:
 
     nivel = 1
-    experience = 0 # min: 0 - max: 99
+    experience = 0  # min: 0 - max: 99
     probability = 0
 
     def __init__(self, name):
@@ -15,28 +16,28 @@ class Character:
         print(f"NIVEL: {self.nivel}", end="\t")
         print(f"EXPERIENCIA: {self.experience}")
 
-    @estado.setter
-    def estado(self, experience):
+    def estado(self, exp):
+        tmp_exp = self.experiencia + exp
 
-        escala = experience // 100
+        while tmp_exp >= 100:
+            self.nivel += 1
+            tmp_exp -= 100
 
-        if experience < 0:
-            self.experience -= abs(experience)
-            if self.experience <= 0:
+        while tmp_exp < 0:
+            if self.nivel > 1:
+                tmp_exp = 100 + tmp_exp
                 self.nivel -= 1
-                self.experience = 0
-
-        elif experience > 0:
-            self.experience += (experience % 100)
-            self.nivel += escala
+            else:
+                tmp_exp = 0
+        self.experiencia = tmp_exp
 
     @staticmethod
     def acciones(probabilidad: int):
-        print(leer_dialogos('probabilidad') % probabilidad)
-        print(leer_dialogos('si_ganas'))
-        print(leer_dialogos('si_pierdes'))
-        print(leer_dialogos('jugar'))
-        return input('> ')
+        print(leer_dialogos("probabilidad") % probabilidad)
+        print(leer_dialogos("si_ganas"))
+        print(leer_dialogos("si_pierdes"))
+        print(leer_dialogos("jugar"))
+        return input("> ")
 
     def probabilidad_de_ganar(self, other):
         if self > other:
@@ -46,13 +47,14 @@ class Character:
         elif self == other:
             self.probability = 50
 
-        return f'{self.probability}%'
+        return f"{self.probability}%"
 
     def __gt__(self, other):
         return self.nivel > other.nivel
+
     def __lt__(self, other):
         return self.nivel < other.nivel
-     
+
     def __eq__(self, other):
         return self.nivel == other.nivel
 
@@ -67,5 +69,3 @@ if __name__ == "__main__":
     # p1.actualizar_estado(250)
     # p1.actualizar_estado(-201)
     # print(p1.consultar_estado())
-
-
---
+++ readme.md
@@ -0,0 +1,1 @@
+### Desafío guiado - Gran Fantasía
