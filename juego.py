6850831248377cbbeebf86ecd19cf2511d7e4dbb
enidev911 <enidev911@gmail.com>
2024-05-29 18:41:36 -0400

    up

--- juego.py
+++ juego.py
@@ -10,26 +10,28 @@ if __name__ == "__main__":
     clear = lambda: os.system("cls" if os.name == "nt" else "clear")
     p1 = Personaje(input())
     orco = Personaje("Orco")
-    jugar = 1
 
-    while jugar == 1:
+    while True:
 
         print(p1.estado)
         jugar = Personaje.jugar(p1.probabilidad_de_ganar(orco) * 100)
 
-        if random.uniform(0, 1) < p1.probabilidad:
-            p1.estado = 50
-            orco.estado = -30
-            clear()
-            print(leer_dialogos("ganado"))
-            time.sleep(2)
-            clear()
+        if jugar == 1:
+            if random.uniform(0, 1) < p1.probabilidad:
+                p1.estado = 50
+                orco.estado = -30
+                clear()
+                print(leer_dialogos("ganado"))
+                time.sleep(2)
+                clear()
+            else:
+                p1.estado = -30
+                orco.estado = 50
+                clear()
+                print(leer_dialogos("perdido"))
+                time.sleep(2)
+                clear()
         else:
-            p1.estado = -30
-            orco.estado = 50
-            clear()
-            print(leer_dialogos("perdido"))
-            time.sleep(2)
-            clear()
+            break
 
 print("¡Has huido! El orco ha quedado atrás.")
